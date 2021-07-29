7cf4740451988ddcd46b083a47470f6628492272
FABRI Ricardo <fabrizio.serial@ing.austral.edu.ar>
2021-07-29 13:52:42 -0300

    feature logo redirect to sirius.com

--- src/home/components/Home/Home.jsx
+++ src/home/components/Home/Home.jsx
@@ -19,7 +19,7 @@ const Home = (props) => {
   return (
     <div className='home-div-background'>
       <div className='home-div-cont-logo'>
-        <img className='home-img-logo' src={logo}/>
+        <img className='home-img-logo' src={logo} onClick={()=>window.location.href='https://sirius.com.ar/'}/>
       </div>
       <div className='home-div-btngroups'>
         <SiriusButton type='fb'/>
@@ -28,12 +28,18 @@ const Home = (props) => {
         <SiriusButton type='wb'/>
       </div>
       {
-        // Hola
+        //    _____ _      _              _____        __ _
+        //   / ____(_)    (_)            / ____|      / _| |
+        //  | (___  _ _ __ _ _   _ ___  | (___   ___ | |_| |___      ____ _ _ __ ___
+        //   \___ \| | '__| | | | / __|  \___ \ / _ \|  _| __\ \ /\ / / _` | '__/ _ \
+        //   ____) | | |  | | |_| \__ \  ____) | (_) | | | |_ \ V  V / (_| | | |  __/
+        //  |_____/|_|_|  |_|\__,_|___/ |_____/ \___/|_|  \__| \_/\_/ \__,_|_|  \___|
+        //
       }
       <div className='home-div-decoration'>
-          {
-              ///<img src={deco} className='home-img-decoration'/>
-          }
+        {
+          // /<img src={deco} className='home-img-decoration'/>
+        }
       </div>
     </div>
   );
